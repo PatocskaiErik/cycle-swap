9ddf877f3742e15b7651132e958aa2a19cfefd6b
Erik <patoerik93@gmail.com>
2023-06-22 17:35:21 +0200

    Added calculations

--- src/main/java/com/epam/rd/autotasks/CycleSwap.java
+++ src/main/java/com/epam/rd/autotasks/CycleSwap.java
@@ -2,10 +2,30 @@ package com.epam.rd.autotasks;
 
 class CycleSwap {
     static void cycleSwap(int[] array) {
+        if(array.length <= 1) {
+            return;
+        }
+    int lastElement = array[array.length - 1];
 
+        for (int i = array.length - 1; i > 0; i--) {
+            array[i] = array[i - 1];
+        }
+        array[0] = lastElement;
     }
 
     static void cycleSwap(int[] array, int shift) {
+        if(array.length <= 1) {
+            return;
+        }
+        shift %= array.length;
 
+        if(shift == 0) {
+            return;
+        }
+        int[] temp = new int[shift];
+
+        System.arraycopy(array, array.length - shift, temp, 0, shift);
+        System.arraycopy(array, 0, array, shift, array.length - shift);
+        System.arraycopy(temp,0, array, 0, shift);
     }
 }
